24a8fce4d25d2b498d86aa46f67c15de6cce4777
paulollerton <paul.ollerton@gmail.com>
2019-06-25 11:15:28 -0700

    inagural

---
+++ src/views/Recipes/Index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <h1>All Recipes</h1>
+    <div v-for="recipe in recipes">
+      <h2>{{ recipe.title }}</h2>
+      <img v-bind:src="recipe.image_url">
+      <p>Category: {{ recipe.category }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      recipes: []
+    };
+  },
+  created: function() {
+    axios.get("/api/recipes").then(response => {
+      this.recipes = response.data;
+    });
+  },
+  methods: {}
+};
+</script>
